43b97f4a2f93cffcc3522ab076e42f09e8e48dc1
Vova Galchenko <vova@galchenko.com>
2014-08-27 16:03:24 -0700

    tagged_message -> tagged_text for consistency

--- controllers/message/createResource.js
+++ controllers/message/createResource.js
@@ -12,20 +12,20 @@ var CreateMessageController = function(resourceId, log) {
   this.feedId = Parameter.String(true)
     .apiAlias('feed_id')
     .description('the feed id for the feed where this message is being posted');
-  this.taggedMessage = Parameter.String(true)
-    .apiAlias('tagged_message')
+  this.taggedText = Parameter.String(true)
+    .apiAlias('tagged_text')
     .description('the textual message that is being posted');
 
   this.run = function(authenticatedUser, responseFactory) {
     var me = this;
-    return feedPermissionChecker.isUserAuthorizedToPost(authenticatedUser, this.feedId)
+    return feedPermissionChecker.isUserAuthorizedToPost(authenticatedUser, me.feedId)
       .then(function(feed) {
         if (feed) {
           return me._persistMessage(authenticatedUser, feed).then(function(message) {
             return responseFactory.success(message);
           });
         } else {
-          throw exceptions.Forbidden('You are not authorized to post messages to feed <' + this.feedId + '>');
+          throw exceptions.Forbidden('You are not authorized to post messages to feed <' + me.feedId + '>');
         }
       });
   }
@@ -34,7 +34,7 @@ var CreateMessageController = function(resourceId, log) {
     return MessagePersistence.persistMessage({
       feedId: this.feedId,
       authorUserId: authenticatedUser.id,
-      taggedText: this.taggedMessage
+      taggedText: this.taggedText
     }).then(function(message) {
       return modelManager.saveModel(feed, {
         'last_message_id' : message.message_id
